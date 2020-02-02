2cb39ae24a34a1e24a1c7180b60688f498561969
valentinaloreto <vloretocastillo@gmail.com>
2020-02-02 12:01:04 +0100

    Updates

---
+++ precache-manifest.4f6ca4a62472d136495d6ce9b070df26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "900bfa80614d3125409057f79cd04e7d",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "c0989d0257fc6b285e0a",
+    "url": "/memory/static/css/main.c08d4897.chunk.css"
+  },
+  {
+    "revision": "1a97612e154dab015ba2",
+    "url": "/memory/static/js/2.cd074799.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/memory/static/js/2.cd074799.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c0989d0257fc6b285e0a",
+    "url": "/memory/static/js/main.ce6961bb.chunk.js"
+  },
+  {
+    "revision": "7f674deac29df26e0ab6",
+    "url": "/memory/static/js/runtime-main.e12c8972.js"
+  }
+]);
